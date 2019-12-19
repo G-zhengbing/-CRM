6b1664b4e5e9bb362106d6bbcd461de0c511b12a
g_zhengbing <g_zhengbing@163.com>
2019-12-19 13:34:33 +0800

    上传

--- qpzx-v2.0/src/main.js
+++ qpzx-v2.0/src/main.js
@@ -1,15 +1,24 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store/index'
+import echarts from 'echarts'
+import ViewUI from 'view-design';
+import 'view-design/dist/styles/iview.css';
+import './uilt/http/index'
+
+Vue.use(ViewUI)
+
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  store,
+  components: {
+    App
+  },
   template: '<App/>'
 })
--- qpzx-v2.0/src/router/index.js
+++ qpzx-v2.0/src/router/index.js
@@ -1,15 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Login from '../components/login/Login'
+import Forget from '../components/login/Forget'
+import Succeed from '../components/login/Succeed'
+import Main from '../components/cms/Main'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
+const originalPush = Router.prototype.push
+Router.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+const router = new Router({
+  routes: [{
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Login',
+      component: Login
+    },
+    {
+      path: '/forget',
+      component: Forget
+    },
+    {
+      path: '/succeed',
+      component: Succeed
+    },
+    {
+      path:'/main',
+      component:Main,
+      children:[]
     }
   ]
 })
+
+export default router
